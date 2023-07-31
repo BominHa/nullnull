6c5fa0791ccaec7fbca193fff917c9b377eb0873
BominHa <122842180+BominHa@users.noreply.github.com>
2023-07-31 21:07:35 +0900

    Add files via upload

---
+++ p.88 3.1.py
@@ -0,0 +1,18 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = 2
+>>> n = 2
+>>> for a in range(2, 7, 1):
+...     print(a,' ',n,' ',a**n)
+... 
+...     
+2   2   4
+3   2   9
+4   2   16
+5   2   25
+6   2   36
+
+
+
+C:\Users\Administrator\Desktop\따라하며\chapter.3 심화문제 풀이C:\Users\Administrator\Desktop\따라하며\chapter.3 심화문제 풀이\p.88 3.1.py
+C:\Users\Administrator\Desktop\따라하며\chapter.3 심화문제 풀이\p.88 3.1.pyfg
